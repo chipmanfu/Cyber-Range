73fe88e45d134a6f6d379fedb6cdb230ad216b40
chipmanfu <mcelvain1@gmail.com>
2023-03-09 14:14:42 -0600

    updated ubuntubuild.sh

--- ubuntubuild.sh
+++ ubuntubuild.sh
@@ -361,9 +361,9 @@ case $opt in
      clear
      echo "We need to set up SSH keys to connect to the CA server to create SSL certs"
      echo "At the prompt use password 'toor'"
-     sshpass -p toor ssh -o StrictHostKeyChecking=no 180.1.1.50  `/root/scripts/certmaker.sh -q -d dropbox.com -C US -ST 'New York' -L 'New York City' -O 'Dropbox,inc' -CN dropbox.com -A dropbox -DNS1 www.dropbox.com`
-     sshpass -p toor ssh 180.1.1.50 `/root/scripts/certmaker.sh -q -d pastebin.com -c US -ST Utah -L Provo -O PasteBin -CN pastebin.com -A pastebin -DNS1 www.pastebin.com`
-     sshpass -p toor ssh 180.1.1.50 `/root/scripts/certmaker.sh -q -d redbook.com -C US -ST Hawaii -L 'big Island' -O 'things corp' -CN redbook.com -A redbook -DNS1 www.redbook.com`
+     sshpass -p toor ssh -o StrictHostKeyChecking=no 180.1.1.50  "/root/scripts/certmaker.sh -q -d dropbox.com -C US -ST 'New York' -L 'New York City' -O 'Dropbox,inc' -CN dropbox.com -A dropbox -DNS1 www.dropbox.com"
+     sshpass -p toor ssh 180.1.1.50 "/root/scripts/certmaker.sh -q -d pastebin.com -c US -ST Utah -L Provo -O PasteBin -CN pastebin.com -A pastebin -DNS1 www.pastebin.com"
+     sshpass -p toor ssh 180.1.1.50 "/root/scripts/certmaker.sh -q -d redbook.com -C US -ST Hawaii -L 'big Island' -O 'things corp' -CN redbook.com -A redbook -DNS1 www.redbook.com"
      sshpass -p toor scp -r 180.1.1.50:/var/www/html/dropbox* /root/owncloud/SSL
      sshpass -p toor scp -r 180.1.1.50:/var/www/html/pastebin* /root/pastebin/SSL
      sshpass -p toor scp -r 180.1.1.50:/var/www/html/redbook* /root/redbook/SSL 
