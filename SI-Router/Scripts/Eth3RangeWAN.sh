2e4bbe899566baec71bfb681d5d9a45be8254e60
chipmanfu <50666533+chipmanfu@users.noreply.github.com>
2023-12-02 20:04:00 -0600

    Update Eth3RangeWAN.sh

--- SI-Router/Scripts/Eth3RangeWAN.sh
+++ SI-Router/Scripts/Eth3RangeWAN.sh
@@ -4,7 +4,5 @@ source /opt/vyatta/etc/functions/script-template
 
 configure
 set interfaces ethernet eth3 address 1.1.1.1/29
-set interfaces ethernet eth3 duplex auto
-set interfaces ethernet eth3 speed auto 
 commit
 exit
