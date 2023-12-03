2aec652718768cf912865c9d628882808f2bf705
chipmanfu <50666533+chipmanfu@users.noreply.github.com>
2023-12-02 20:04:15 -0600

    Update Eth0AdminConfig.sh

--- SI-Router/Scripts/Eth0AdminConfig.sh
+++ SI-Router/Scripts/Eth0AdminConfig.sh
@@ -4,9 +4,6 @@ source /opt/vyatta/etc/functions/script-template
 
 configure
 set interfaces ethernet eth0 address 172.30.7.254/24
-set interfaces ethernet eth0 duplex auto
-set interfaces ethernet eth0 speed auto 
-
 commit
 save
 exit
